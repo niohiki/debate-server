878b055c7483615fd66a6c6bcf3e4d651adc9ab1
Santiago Codesido Sanchez <Santiago.Codesido@unige.ch>
2014-10-18 19:13:28 +0200

    Added basic chrono creator with random ids

--- src/org/niohiki/debateserver/servlets/ChronoServlet.java
+++ src/org/niohiki/debateserver/servlets/ChronoServlet.java
@@ -1,15 +1,12 @@
 package org.niohiki.debateserver.servlets;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Date;
+import java.security.SecureRandom;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Map.Entry;
-import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -20,12 +17,12 @@ import org.niohiki.debateserver.Locale;
 import org.niohiki.debateserver.chronometer.Chronometer;
 import org.niohiki.debateserver.html.Body;
 import org.niohiki.debateserver.html.CSSLink;
-import org.niohiki.debateserver.html.ClickableDiv;
 import org.niohiki.debateserver.html.Div;
 import org.niohiki.debateserver.html.Form;
 import org.niohiki.debateserver.html.HTML;
 import org.niohiki.debateserver.html.Head;
 import org.niohiki.debateserver.html.Input;
+import org.niohiki.debateserver.html.Tag;
 
 public class ChronoServlet extends HttpServlet {
 
@@ -33,11 +30,13 @@ public class ChronoServlet extends HttpServlet {
     private final Configuration configuration;
     private final Locale locale;
     private final String md5password;
+    private final SecureRandom random;
 
     public ChronoServlet(Configuration conf, Locale loc, String md5pass) {
         configuration = conf;
         locale = loc;
         md5password = md5pass;
+        random = new SecureRandom();
         chronometers = new HashMap<>();
         Chronometer chronometer = new Chronometer(conf);
         chronometer.mainReset();
@@ -57,8 +56,23 @@ public class ChronoServlet extends HttpServlet {
                     request.getSession().setAttribute("chrono_authenticated", "1");
                     response.sendRedirect((String) request.getSession().
                             getAttribute("login_redirection"));
+                } else {
+                    response.sendRedirect("/chrono");
                 }
             } catch (NoSuchAlgorithmException ex) {
+            }
+        } else {
+            HttpSession httpSession = request.getSession();
+            if ("1".equals(httpSession.getAttribute("chrono_authenticated"))) {
+                if ("1".equals(request.getParameter("create"))) {
+                    Chronometer newChronometer = new Chronometer(configuration);
+                    String id;
+                    do {
+                        id = new BigInteger(130, random).toString(32);
+                    } while (chronometers.keySet().contains(id));
+                    chronometers.put(id, newChronometer);
+                    response.sendRedirect("/chrono");
+                }
             }
         }
     }
@@ -80,7 +94,11 @@ public class ChronoServlet extends HttpServlet {
         } else {
             HttpSession httpSession = request.getSession();
             if ("1".equals(httpSession.getAttribute("chrono_authenticated"))) {
-
+                if ("1".equals(request.getParameter("new"))) {
+                    response.setContentType("text/html");
+                    response.setStatus(HttpServletResponse.SC_OK);
+                    response.getWriter().println(makeNew());
+                }
             } else {
                 request.getSession().setAttribute("login_redirection",
                         request.getRequestURI() + "?" + request.getQueryString());
@@ -92,42 +110,59 @@ public class ChronoServlet extends HttpServlet {
     }
 
     private String makeChronoList() {
-        Div[] items = new Div[chronometers.size() + 1];
-        items[0] = new Div("select_item", "", "",
-                new ClickableDiv("select_text select_button", "location.href='chrono?new=1'",
-                        "", locale.chrono.newChrono)
+        Tag[] items = new Div[chronometers.size() + 1];
+        items[0] = new Div("select_item").child(
+                new Div("select_text select_button").
+                content(locale.chrono.newChrono).
+                attribute("onClick", "location.href='chrono?new=1'")
         );
         Iterator<String> keys = chronometers.keySet().iterator();
         for (int i = 1; i < items.length; i++) {
             String key = keys.next();
             Chronometer chrono = chronometers.get(key);
-            items[i] = new Div("select_item", "", "",
-                    new Div("select_text", "", key),
-                    new Div("select_text select_button", "", locale.chrono.see),
-                    new Div("select_text select_button", "", locale.chrono.control)
+            items[i] = new Div("select_item").child(
+                    new Div("select_text").content(key),
+                    new Div("select_text select_button").content(locale.chrono.see),
+                    new Div("select_text select_button").content(locale.chrono.control)
             );
         }
-        return new HTML(
-                new Head(
+        return new HTML().child(
+                new Head().child(
                         new CSSLink("/static/chrono.css")
                 ),
-                new Body(
-                        new Div("select_list", "", "",
-                                items
-                        )
+                new Body().child(
+                        new Div("main").child(items)
                 )
         ).toHTML();
     }
 
     private String makePassword() {
-        return new HTML(
-                new Head(
-                        new CSSLink("/static/chrono.css")
-                ),
-                new Body(
-                        new Form("password_form", "post", "chrono",
-                                new Input("password_text", "password", "", "password"),
-                                new Input("", "login", "1", "hidden")
+        return new HTML().child(
+                new Head().child(new CSSLink("/static/chrono.css")),
+                new Body().child(
+                        new Form("post", "chrono").attribute("class", "password_form").child(
+                                new Input().
+                                attribute("name", "password").attribute("type", "password").
+                                attribute("value", "").attribute("class", "password_text"),
+                                new Input().
+                                attribute("name", "login").attribute("type", "hidden").
+                                attribute("value", "1")
+                        )
+                )
+        ).toHTML();
+    }
+
+    private String makeNew() {
+        return new HTML().child(
+                new Head().child(new CSSLink("/static/chrono.css")),
+                new Body().child(
+                        new Form("post", "chrono").attribute("class", "password_form").child(
+                                new Input().
+                                attribute("name", "name").attribute("type", "text").
+                                attribute("value", "").attribute("class", "password_text"),
+                                new Input().
+                                attribute("name", "create").attribute("type", "hidden").
+                                attribute("value", "1")
                         )
                 )
         ).toHTML();
