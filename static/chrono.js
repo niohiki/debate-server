b653338a1da02436d12a9fd7bb90bbc24f1d4ea9
Santiago Codesido Sanchez <Santiago.Codesido@unige.ch>
2014-10-19 20:19:40 +0200

    Fixed minor issue with seconds format and set synchronous mode

--- static/chrono.js
+++ static/chrono.js
@@ -9,22 +9,24 @@ function parseNanoTime(input) {
     var sign = input / module;
     var seconds = (module - (module % (1000 * 1000 * 1000))) / (1000 * 1000 * 1000);
     var minutes = sign * (seconds - (seconds % 60)) / 60;
-    return minutes + ":" + (seconds % 60);
+    seconds = seconds % 60;
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    return minutes + ":" + seconds;
 }
 function readapt(id) {
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-        var answer = JSON.parse(xmlhttp.responseText);
-        var displayString = parseNanoTime(answer.mainTime)
-        if (answer.secondaryTime > 0) {
-            displayString += " + " + parseNanoTime(answer.secondaryTime);
-        }
-        document.getElementById("display").innerHTML = displayString;
-        document.getElementById("name").innerHTML = answer.name;
-        document.getElementById("stance").innerHTML = answer.stance;
-    };
-    xmlhttp.open("GET", "/chrono?get=1&id=" + id, true);
+    xmlhttp.open("GET", "/chrono?get=1&id=" + id, false);
     xmlhttp.send();
+    var answer = JSON.parse(xmlhttp.responseText);
+    var displayString = parseNanoTime(answer.mainTime)
+    if (answer.secondaryTime > 0) {
+        displayString += " + " + parseNanoTime(answer.secondaryTime);
+    }
+    document.getElementById("display").innerHTML = displayString;
+    document.getElementById("name").innerHTML = answer.name;
+    document.getElementById("stance").innerHTML = answer.stance;
 
     var display = document.getElementById("display");
     var chrono = document.getElementById("chrono");
