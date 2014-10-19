22146af8ea66fc27107d0391194fa426a2449305
Santiago Codesido Sanchez <Santiago.Codesido@unige.ch>
2014-10-19 19:48:28 +0200

    JS for chrono watch (full)

--- static/chrono.js
+++ static/chrono.js
@@ -4,7 +4,28 @@ function widthOf(element) {
 function heightOf(element) {
     return Number(window.getComputedStyle(element).height.replace(/[^\d\.\-]/g, ''));
 }
-function readapt() {
+function parseNanoTime(input) {
+    var module = Math.abs(input);
+    var sign = input / module;
+    var seconds = (module - (module % (1000 * 1000 * 1000))) / (1000 * 1000 * 1000);
+    var minutes = sign * (seconds - (seconds % 60)) / 60;
+    return minutes + ":" + (seconds % 60);
+}
+function readapt(id) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        var answer = JSON.parse(xmlhttp.responseText);
+        var displayString = parseNanoTime(answer.mainTime)
+        if (answer.secondaryTime > 0) {
+            displayString += " + " + parseNanoTime(answer.secondaryTime);
+        }
+        document.getElementById("display").innerHTML = displayString;
+        document.getElementById("name").innerHTML = answer.name;
+        document.getElementById("stance").innerHTML = answer.stance;
+    };
+    xmlhttp.open("GET", "/chrono?get=1&id=" + id, true);
+    xmlhttp.send();
+
     var display = document.getElementById("display");
     var chrono = document.getElementById("chrono");
     var fs = 10;
@@ -14,4 +35,6 @@ function readapt() {
         fs *= 1.1;
     }
     chrono.style.marginTop = ((window.innerHeight - heightOf(chrono)) * 0.4) + "px";
+
+
 }
